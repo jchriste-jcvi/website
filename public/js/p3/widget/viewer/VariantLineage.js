ecab62ef3b34d0e4c39e4ef33751107799a672aa
Harry Yoo <hsyoo@anl.gov>
2021-04-15 13:11:57 -0500

    hide structure tab temporarily

--- public/js/p3/widget/viewer/VariantLineage.js
+++ public/js/p3/widget/viewer/VariantLineage.js
@@ -135,10 +135,6 @@ define([
         title: 'Genome Browser',
         id: this.viewer.id + '_jbrowse'
       })
-      this.structure = new VariantProteinStructure({
-        title: 'Protein Structure',
-        id: this.viewer.id + '_structure'
-      })
 
       /*
       this.structure = new VariantStructure({
